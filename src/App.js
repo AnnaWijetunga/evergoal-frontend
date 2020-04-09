3d42d3299ee4963a03267c29511f81e9f10803fa
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-09 16:20:10 -0400

    Use componentDidMouont to fetch array of objects from the back end - success

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  // DELETE at some point
+  // testing to see if I can fetch data from the back end 
+  // success! fetched my array of 2 objects! too cool!
+  componentDidMount() {
+    fetch('http://localhost:3000/api/goals')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">
