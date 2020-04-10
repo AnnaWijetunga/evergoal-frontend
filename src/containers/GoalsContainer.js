bace241a83d9de2db57432a6cb8e465bacab67c1
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-10 13:53:24 -0400

    Add notes about connect and its purpose

--- src/containers/GoalsContainer.js
+++ src/containers/GoalsContainer.js
@@ -9,6 +9,7 @@ import { fetchGoals, deleteGoal } from '../actions/goalsActions';
 
 class GoalsContainer extends Compoent {
 
+    // 
     componentDidMount(){
         this.props.fetchGoals()
     }
@@ -33,6 +34,9 @@ const mapDispatchToProps = dispatch => ({
 })
 
 // connect to our store
+// this connect automatically calls store.dispatch for us on the return values of our mapDisptachToProps (fetchGoals and deleteGoal)
+// what's passed to our dispatch is our action object
+// we don't SEE dispatch because it happen within connect automatically
 // mapStateToProps: we want access to what's in the store
 // mapDispatchToProps: we want the ability to dispatch new actions to our store from our component
 export default connect(mapStateToProps, mapDispatchToProps)(GoalsContainer);
