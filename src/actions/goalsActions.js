a65a52ff5f405ddd3e2f9a11087c7017029c9ac7
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-10 14:36:19 -0400

    Remind me of the fetch request process

--- src/actions/goalsActions.js
+++ src/actions/goalsActions.js
@@ -29,3 +29,13 @@ export const fetchGoal = id => {
 
 // delete goal
 
+
+
+// Reminder of fetch requests:
+// this fetch request returns a promise.
+// a promise that we’ll get some sort of response eventually.
+// once that promise is resolved, the response from our request comes into our first THEN.
+// ee convert that into JSON.
+// once that’s converted to JSON, that comes down as the argument in the next THEN.
+// accounts is what we’re hoping to get back - the same as that converted response to JSON.
+// dispatching - go to our reducer, take our reducer this action object and update our Redux store based on the info I’m sending you here (type/payload).
