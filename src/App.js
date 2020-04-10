d8e2825b34aa5a0abcf4abf56a793abac2392de3
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-10 08:55:50 -0400

    Update api url to retrieve nested data - objectives nested under goals

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   // testing to see if I can fetch data from the back end 
   // success! fetched my array of 2 objects! too cool!
   componentDidMount() {
-    fetch('http://localhost:3000/api/goals')
+    fetch('http://localhost:3000/api/goals/1/objectives')
     .then(response => response.json())
     .then(data => console.log(data))
   }
