20047bb89fdcd2dfeb7e4a514040bcd403964eb3
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-10 11:59:05 -0400

    Use combineReducer to combine goalsReducer and objectiveReducer and import combineReducers as well as both reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import goalsReducer from './manageGoals';
+import objectivesReducer from './manageObjectives';
+
+const rootReducer = combineReducers({
+    goals: goalsReducer,
+    objectives: objectivesReducer
+})
+
+export default rootReducer;
