cc74ebe13cfdb4aa5992fcafc3ffca59cd3e7f77
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-10 10:38:36 -0400

    Comment out componentDidMount fetch request - no longer needed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 
 class App extends React.Component {
 
-  // DELETE at some point
   // testing to see if I can fetch data from the back end 
   // success! fetched my array of 2 objects! too cool!
-  componentDidMount() {
-    fetch('http://localhost:3000/api/goals/1/objectives')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/goals/1/objectives')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }
 
   render() {
     return (
