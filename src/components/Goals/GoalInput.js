5add2485323b461fa01ce830d2215aec99ab5a36
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-10 14:24:19 -0400

    Test to see if we're rendering this component

---
+++ src/components/Goals/GoalInput.js
@@ -0,0 +1,24 @@
+// class component
+import React, { Component } from 'react';
+
+import { addGoal } from '../../actions/goalsActions';
+import { connect } from 'react-redux';
+
+class GoalInput extends Component {
+    
+    // correct syntax here?
+    // state = {
+    //     aim: "",
+    //     strategy: ""
+    // }
+
+    render() {
+        return(
+           <div>
+               GoalInput
+           </div> 
+        )
+    }
+}
+
+export default connect(null, { addGoal })(GoalInput);
