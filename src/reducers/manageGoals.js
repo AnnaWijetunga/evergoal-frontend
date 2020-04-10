bc2a9480a5aec686fc392142263e80ab63c6606d
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-10 11:54:55 -0400

    Start mapping out the reducer - default state, action and the case statements for each action

---
+++ src/reducers/manageGoals.js
@@ -0,0 +1,31 @@
+// a reducer is a function
+
+
+// the first argument is state to set initial state
+// object with a goalsData key pointing to an empty array
+// object with a goalData key pointing to an empty array
+// the second argument is action
+export default function goalsReducer(state = {
+    loading: false,
+    goalsData: [],
+    goalData: []
+    }, action){
+
+        // have to complete the return
+        switch(action.type){
+            case 'CREATE_GOAL':
+                console.log('creating', action.payload)
+                return { }
+            case 'LOADING_GOALS':
+                console.log("loading goals here")
+                return { }
+            case 'FETCH_GOALS':
+                console.log("fetching goals")
+                return { }
+            case 'DELETE_GOAL':
+                console.log("in reducer, deleting", action.payload)
+            default:
+                console.log("default case in goals reducer")
+                return state;
+        }
+}
