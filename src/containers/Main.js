baa476577991df78330fb01292c7ad316c778fe5
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-11 12:56:22 -0400

    Add exact routes for 4 paths

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import NavBar from '../components/Navbar';
 import { Route } from 'react-router-dom';
-// import Home from '../components/Home';
-// import GoalInput from '../components/Goals/GoalInput';
-// import GoalsContainer from './GoalsContainer';
-// import ObjectivesContainer from './ObjectivesContainer';
+import Home from '../components/Home';
+import GoalInput from '../components/Goals/GoalInput';
+import GoalsContainer from './GoalsContainer';
+import ObjectivesContainer from './ObjectivesContainer';
 import MainHeader from '../components/MainHeader';
 import { Container, Header } from 'semantic-ui-react';
 
@@ -18,12 +18,10 @@ class Main extends Component {
                     <NavBar />
 
                     <Container>
-                        
-                        <Route  />
-                        <Route  />
-                        <Route  />
-                        <Route  />
-
+                        <Route exact path='/' render={Home} />
+                        <Route exact path='/goals/new' component={GoalInput} />
+                        <Route exact path='/goals' component={GoalsContainer} />
+                        <Route exact path='/goals/:goalId/objectives' component={ObjectivesContainer} />
                     </Container>
                 </Container>
             </div>
