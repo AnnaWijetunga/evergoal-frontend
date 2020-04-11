2ee7b332a1bf8fb913069f9819aca512647f3821
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-11 11:01:01 -0400

    Create template for the main header of the Homepage and beyond

---
+++ src/components/MainHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import Clock -- if using this
+// <Clock /> -- would add this component below <h1> tag, if using
+
+const MainHeader = () => (
+    <div className="App-header">
+        <h1>Evergoal</h1>
+    </div>
+)
+
+export default MainHeader;
