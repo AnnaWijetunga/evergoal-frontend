9ce9a5d3c289ae71bf912dd367d226709cdbfb29
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-11 11:00:20 -0400

    Build out the delete goal action creator - allowing a goal to be deleted

--- src/actions/goalsActions.js
+++ src/actions/goalsActions.js
@@ -46,7 +46,24 @@ export const addGoal = goalInput => {
 }
 
 // delete goal
-
+export const deleteGoal = goal_id => {
+    let data = {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+    return dispatch => {
+        fetch(`${goalsURL}/${goal_id}`, data)
+            .then(response => response.json())
+            .then(goal => dispatch({
+                type: 'DELETE_GOAL',
+                payload: goal
+            }))
+            .catch(err => err)
+    }
+}
 
 // Reminder of fetch requests:
 // this fetch request returns a promise.
