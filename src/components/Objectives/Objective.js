33604919e4cad2a4ea967074deaf2e4468fbbb62
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-11 14:24:47 -0400

    Create handleCheck and return an edit, delete and completed options for objectives

---
+++ src/components/Objectives/Objective.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Checkbox, Icon } from 'semantic-ui-react';
+
+export default class Objective extends Component {
+
+    handleCheck = () => {
+        this.props.toggleCompleted(this.props.id)
+    }
+
+    render() {
+        return(
+            <div>
+                <Icon className="pointer" name='edit' color='blue' onClick={() => console.log("editing right now")} />
+                
+                <Icon className="pointer" name='delete' color='red' onClick={() => this.props.deleteObjective(this.props.id)} />
+                
+                <Checkbox value={this.props.completed} checked={!!this.props.completed} onChange={(event) => this.handleCheck(event)} />{this.props.description}
+            </div>
+        )
+    }
+}
