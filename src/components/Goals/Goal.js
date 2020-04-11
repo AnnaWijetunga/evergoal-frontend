4554efbc3ddf2fc6f007cda2d80181874d4b8e5b
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-11 13:54:11 -0400

    Add objectives link and delete button

---
+++ src/components/Goals/Goal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+
+
+const Goal = ({ aim, deleteGoal, id }) => {
+    return(
+        <div className="goal">
+            <NavLink key={id} to={`/goals/${id}/objectives`}><h3>{aim}</h3></NavLink>
+            <Button className="delete-button" basic color='red' size='mini' onClick={() => deleteGoal(id)}>Delete</Button>
+        </div>
+    )
+}
+
+export default Goal;
