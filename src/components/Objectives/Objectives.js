90afbcb3038b55ee74579655c6b110e3d3cf8736
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-11 15:11:02 -0400

    Create objectives list with attributes

---
+++ src/components/Objectives/Objectives.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Objective from './Objective';
+
+const Objectives = (props) => {
+    const objectivesList = props.objectives.map(objective =>
+        <li className="objective-items">
+            <Objective 
+                key={objective.id}
+                id={objective.id}
+                completed={objective.completed}
+                description={objective.description}
+                deleteObjective={props.deleteObjective}
+                toggleCompleted={props.toggleCompleted}
+                />
+        </li>)
+
+    return (
+        <div className="objectives-box">
+            <h1>{props.goal.aim}</h1>
+            <label><strong>>The Plan</strong></label>
+            <p>{props.goal.strategy}</p>
+            <strong>Key Objectives</strong>
+            <ul>
+                {objectivesList}
+            </ul>
+        </div>
+    );
+};
+
+export default Objectives;
