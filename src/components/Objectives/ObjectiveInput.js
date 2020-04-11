86ea8fb1f3e20d4f9dfda89d33c7863a33bedb5a
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-11 14:14:51 -0400

    Create handleChange, handleOnSubmit, and a form for new objectives

---
+++ src/components/Objectives/ObjectiveInput.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Button, Form } from 'semantic-ui-react}';
+
+class ObjectiveInput extends Component {
+
+    state = {
+        description: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        this.props.addObjective(this.state, this.props.goalId);
+        this.setState({
+            description: ""
+        })
+    }
+
+    render() {
+        return(
+            <Form inverted className="new-objective form" onSubmit={(event) => this.handleOnSubmit(event)}>
+                <Form.Field>
+                    <label className="form-label">Add Objective</label>
+                    <input placeholder="List milestones to reach your goal" id="description" required value={this.state.description} onChange={(event) => this.handleChange(event)} />            
+                </Form.Field>
+
+                <Button size='mini' type="submit">Submit</Button>
+            </Form>
+        )
+    }
+}
+
+export default ObjectiveInput;
