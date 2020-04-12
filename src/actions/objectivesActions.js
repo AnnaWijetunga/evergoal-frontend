4f79d3e8eada53638c2b5b607695a5a0ea6ad1c1
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-12 10:27:32 -0400

    Fetch goal objectives - action creator

--- src/actions/objectivesActions.js
+++ src/actions/objectivesActions.js
@@ -1,7 +1,23 @@
 const goalsURL = "/api/goals";
 
 // fetch goal objectives
-
+export const fetchGoalObjectives = goalId => {
+    return dispatch => {
+        fetch(`${goalsURL}/${goalId}/objectives`)
+            .then(response => response.json())
+            .then(objectives => {
+                let res = {
+                    objectives,
+                    goalId
+                }
+                dispatch({
+                    type: 'FETCH_OBJECTIVES',
+                    payload: res 
+                })
+            })
+            .catch(err => err)
+    }
+}
 
 // create new objective
 
