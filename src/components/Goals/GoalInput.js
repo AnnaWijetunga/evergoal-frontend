97ec345a6023bd2a4a7bfdc3e7fae6fcfc8cb7e7
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-13 13:58:34 -0400

    Describe what happens during handleChange

--- src/components/Goals/GoalInput.js
+++ src/components/Goals/GoalInput.js
@@ -11,6 +11,10 @@ class GoalInput extends Component {
         strategy: ""
     }
 
+    // when someone types, we want to change our state
+    // pass in event as the parameter
+    // abstracting here - id is being used for aim and strategy
+    // brackets around [event.target.id]: evaluationg what's inside, whether it's aim or strategy. First read the value associated and THEN set it as the proper key
     handleChange = (event) => {
         this.setState({
             [event.target.id]: event.target.value
@@ -26,13 +30,15 @@ class GoalInput extends Component {
         })
     }
 
+    // original: onChange for aim
+    // onChange={(event) => this.handleChange(event)} - don't have to pass in the event, it's passed automatically for me
     render() {
         return(
             <Form inverted className="new-goal-form" onSubmit={(event) => this.handleOnSubmit(event)}>
                 <h5>Set a New Goal</h5>
                 <Form.Field>
                     <label className="form-label">Aim</label>
-                    <input placeholder="Your Next Achievement" id="aim" required value={this.state.aim} onChange={(event) => this.handleChange(event)} />
+                    <input placeholder="Your Next Achievement" id="aim" required value={this.state.aim} onChange={this.handleChange} />
                 </Form.Field>
 
                 <Form.Field>
@@ -46,4 +52,7 @@ class GoalInput extends Component {
     }
 }
 
+// this component does not need access to what's already in the Redux store - null for mapStateToProps
+// this is only for adding something new - a new goal
+// we call that action creator - { addGoal } vs mapDispatchToProps
 export default connect(null, { addGoal })(GoalInput);
