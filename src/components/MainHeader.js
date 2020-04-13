af376c7ee492ebb8b2974e7c1292f05a39d8d301
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-13 15:48:17 -0400

    Delete comments - no longer needed

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-// import Clock -- if using this
-// <Clock /> -- would add this component below <h1> tag, if using
-
 const MainHeader = () => (
     <div className="App-header">
         <h1>Evergoal</h1>
