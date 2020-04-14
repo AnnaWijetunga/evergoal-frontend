930c6607f5b943bb7a29b880192d5c7097246d0a
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-14 14:43:02 -0400

    Fix spacing issue

--- src/reducers/manageObjectives.js
+++ src/reducers/manageObjectives.js
@@ -15,7 +15,7 @@ export default function objectivesReducer(state = {
             return {...state, objectivesData:[...state.objectivesData, action.payload.objective]}
         case 'DELETE_OBJECTIVE':
             console.log("deleting objective", action.payload)
-            return {...state, loading: false, objectivesData: state.objectivesData.filter(objective => objective.id !== action.payload.id)}
+            return {...state, loading: false, objectivesData: state.objectivesData.filter(objective => objective.id !== action.payload.id )}
         case 'TOGGLE_COMPLETED_OBJECTIVE':
             console.log("toggling complete objective")
             return {...state, objectivesData: [...state.objectivesData.map(objective => objective.id === action.payload.id ? {...objective, completed: !objective.completed} : objective )]}
