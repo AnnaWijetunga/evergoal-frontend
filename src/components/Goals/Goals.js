0d482b6b8f2a7c7b308ddc3ebe3b67779ff4035b
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-14 08:17:02 -0400

    Give each key its own line - for readability

--- src/components/Goals/Goals.js
+++ src/components/Goals/Goals.js
@@ -8,7 +8,13 @@ import Goal from './Goal';
 import { Container } from 'semantic-ui-react';
 
 const Goals = props => {
-    const goalsList =  props.goals.map(goal => <Goal key={goal.id} goal={goal} {...goal} deleteGoal={props.deleteGoal}/>)
+    const goalsList =  props.goals.map(goal => 
+        <Goal 
+            key={goal.id} 
+            goal={goal} 
+            {...goal} 
+            deleteGoal={props.deleteGoal}/
+        >)
     return (
         <Container>
             {goalsList}
