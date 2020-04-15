eb3971b33fc53d074c0e2f1cf31e01783db14526
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-15 08:16:51 -0400

    Revise notes and update data on what's inside the index file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
+// why import React from 'react'?
+// after Babel transforms your code (which is written with JSX elements), into the React.createElement calls, you can see where React is used (transforming a return into return React.createElement, for example)
+// if you don't import React, it'll be undefined and the createElement call fails
 import React from 'react';
+
+// ReactDOM is a package that provides DOM specific methods that can be used at the top level of a web app to enable an efficient way of managing DOM elements of the web page (like render())
 import ReactDOM from 'react-dom';
+
+// import basic homepage styling from the css file
 import './index.css';
+
+// App contains the Main container, which in turn hosts routes and numerous important components
 import App from './App';
-// allows us to configure routes that show only the components we specify on the page, depending on the route
+
+// allows us to configure routes that show only the components (vs ALL components at once) we specify on the page, depending on the route
 import { BrowserRouter as Router} from 'react-router-dom';
 
-// set up Redux store
+// Redux store setup below
 
 // any component that we wrap in Provider will have access to our Redux store
 import { Provider } from 'react-redux';
-// import createStore to set up our store
-// we add compose here to combine middlewares - so we only pass in 1
+
+// import createStore: sets up our store
+// add compose to combine middlewares - so we only pass in 1 vs multiple
 import { createStore, applyMiddleware, compose } from 'redux';
+
+// with a plain basic Redux store, you can only do simple synchronous updates by dispatching an action
+// middleware extend the store's abilities, and lets you write async logic that interacts with the store
+// middleware for synchronous logic that needs access to teh store and simple async logic (AJAX requests)
 import thunk from 'redux-thunk';
+
 // every time we update something in our store, these reducers return our updated state
 import rootReducer from './reducers/index';
 
-// import * as serviceWorker from './serviceWorker';
-
-// explicitly connectt to our devtools:
+// explicitly connect to our devtools:
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
 compose;
 
@@ -37,11 +51,6 @@ ReactDOM.render((
 document.getElementById('root')
 );
 
-// We’ll send an action object to our reducer. Reducer takes in action object and decides what we want to do update about our current store. Reducer returns new version of that store.
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
-// serviceWorker.register();
+// we’ll send an action object to our reducer
+// reducer takes in action object and decides what we want to do update about our current store
+// reducer returns new version of that store
