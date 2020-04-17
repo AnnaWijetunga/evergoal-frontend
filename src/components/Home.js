778c6c38b33f8f87656d542b04fcb275eefb7043
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-17 08:52:36 -0400

    Clarify Home.js - a link to My Goals

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// add a wee sentence to the Homepage, another place to click to access My Goals
 const Home = () => (
     <div className ="home-page">
         <h3>Add a new goal to your <Link to="/goals">list</Link>!</h3>
