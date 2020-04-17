331b902378e19cc507a914599262d8b631c33308
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-17 08:53:22 -0400

    Clarify Evergoal header for Homepage

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// displays Evergoal at the top of the Homepage - and is always there
 const MainHeader = () => (
     <div className="App-header">
         <h1>Evergoal</h1>
