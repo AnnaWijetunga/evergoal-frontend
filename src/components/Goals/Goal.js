a0dfc5ae5009a5397655d4f32f30ebf481ee9610
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-18 15:53:39 -0400

    Comment out complete button for a goal, as well as class component to handle an event

--- src/components/Goals/Goal.js
+++ src/components/Goals/Goal.js
@@ -3,13 +3,43 @@ import { Button } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
 
-const Goal = ({ aim, deleteGoal, id }) => {
+const Goal = ({ aim, toggleComplete, deleteGoal, id }) => {
     return(
         <div className="goal">
             <NavLink key={id} to={`/goals/${id}/objectives`}><h3>{aim}</h3></NavLink>
+
+            {/* <Button className="complete-button" basic color='green' size='mini' onClick={() => toggleComplete(id)}>Complete</Button> */}
+            
             <Button className="delete-button" basic color='red' size='mini' onClick={() => deleteGoal(id)}>Delete</Button>
         </div>
     )
 }
 
 export default Goal;
+
+
+
+// testing out a new format to incorportate an event listener
+// import React, { Component } from 'react';
+// import { Button } from 'semantic-ui-react';
+// import { NavLink } from 'react-router-dom';
+// import { Checkbox } from 'semantic-ui-react';
+
+// export default class Goal extends Component {
+
+//     handleCheck = () => {
+//         this.props.toggleComplete(this.props.id)
+//     }
+
+//     render() {
+//         return(
+//             <div className="goal">
+//                 <NavLink key={this.props.id} to={`/goals/${id}/objectives`}><h3>{this.props.aim}</h3></NavLink>
+
+//                 <Checkbox value={this.props.complete} checked={!!this.props.complete} onChange={this.handleCheck} />
+
+//                 <Button className="delete-button" basic color='red' size='mini' onClick={() => this.props.deleteGoal(this.props.id)}>Delete</Button>
+//             </div>
+//         )
+//     }
+// }
