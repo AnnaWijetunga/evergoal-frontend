9a60b549e525526658f870abf3bfb0eaf5bd9aba
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-21 08:50:41 -0400

    Shift order of Delete and Complete buttons

--- src/components/Goals/Goal.js
+++ src/components/Goals/Goal.js
@@ -7,10 +7,8 @@ const Goal = ({ aim, deleteGoal, id }) => {
     return(
         <div className="goal">
             <NavLink key={id} to={`/goals/${id}/objectives`}><h3>{aim}</h3></NavLink>
-
-            <Button className="complete-button" basic color='green' size='mini' onClick={() => deleteGoal(id)}>Complete</Button>
-            
-            <Button className="delete-button" basic color='red' size='mini' onClick={() => deleteGoal(id)}>Delete</Button>
+            <Button className="delete-button" color='red' size='mini' onClick={() => deleteGoal(id)}>Delete</Button>
+            <Button className="complete-button" color='green' size='mini' onClick={() => deleteGoal(id)}>Complete</Button>
         </div>
     )
 }
