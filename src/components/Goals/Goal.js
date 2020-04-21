653c577841af67c54c471c8fda9b6c5fb9eb1271
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-21 08:29:28 -0400

    Add complete button to a goal - which deletes the goal

--- src/components/Goals/Goal.js
+++ src/components/Goals/Goal.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Button } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
-
+// complete button deletes the goal
 const Goal = ({ aim, deleteGoal, id }) => {
     return(
         <div className="goal">
             <NavLink key={id} to={`/goals/${id}/objectives`}><h3>{aim}</h3></NavLink>
 
-            {/* <Button className="complete-button" basic color='green' size='mini' onClick={() => toggleComplete(id)}>Complete</Button> */}
+            <Button className="complete-button" basic color='green' size='mini' onClick={() => deleteGoal(id)}>Complete</Button>
             
             <Button className="delete-button" basic color='red' size='mini' onClick={() => deleteGoal(id)}>Delete</Button>
         </div>
